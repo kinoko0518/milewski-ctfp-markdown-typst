945d7645e6a52d3cf18414f71ad74e9f5eed1bfa
kinoko0518 <t4keda6osci01a@gmail.com>
2026-07-15 22:06:36 +0900

    日本語特有のリンク切れを修正

--- convert.sh
+++ convert.sh
@@ -6,9 +6,15 @@ find ./japanese ./english -type f -name "*.md" | while read -r file; do
     echo "Converting: $file -> $outfile"
     mkdir -p "$(dirname "$outfile")"
 
-    pandoc "$file" \
+    /home/mikami-kousei/Softwares/pandoc-3.10/bin/pandoc "$file" \
         -o "$outfile" \
         --lua-filter=tex-to-typst.lua
+
+    # pandocのバグ回避用（#issue 2）
+    # 修正されたら消す
+    perl -pi -e 's/upright\("__COMPOSE__"\)/ compose /g' "$outfile"
+    perl -pi -e 's/upright\("__IN__"\)/ in /g' "$outfile"
+    perl -pi -e 's/upright\("__CONG__"\)/ tilde.equiv /g' "$outfile"
 done
 
 find ./out -mindepth 1 -maxdepth 1 -type d | while read -r dir; do
